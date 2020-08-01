fad99476ef1457bb5f3418be9399cde102739122
Adarsh777 <adarshgoldar8@gmail.com>
2020-08-01 20:21:55 +0530

    Project Created

---
+++ src/components/Hamburger.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import gsap from "gsap";
+
+const Hamburger = ({ state }) => {
+  let menu = useRef(null);
+  let revealMenu = useRef(null);
+  let revealMenuBackground = useRef(null);
+  let line1 = useRef(null);
+  let line2 = useRef(null);
+  let line3 = useRef(null);
+
+  useEffect(() => {
+    if (state.clicked === false) {
+      //close our menu
+      gsap.to([revealMenu, revealMenuBackground], {
+        duration: 0.8,
+        height: 0,
+        ease: "power3.inOut",
+        stagger: {
+          amount: 0.07,
+        },
+      });
+      gsap.to(menu, {
+        duration: 1,
+        css: { display: "none" },
+      });
+    } else if (
+      state.clicked === true ||
+      (state.clicked === true && state.initial === null)
+    ) {
+      // open our menu
+      gsap.to(menu, {
+        duration: 0,
+        css: { display: "block" },
+      });
+      gsap.to([revealMenuBackground, revealMenu], {
+        duration: 0,
+        opacity: 1,
+        height: "100%",
+      });
+      staggerReveal(revealMenuBackground, revealMenu);
+      staggerText(line1, line2, line3);
+    }
+  }, [state]);
+
+  const staggerReveal = (node1, node2) => {
+    gsap.from([node1, node2], {
+      duration: 0.8,
+      height: 0,
+      transformOrigin: "right top",
+      skewY: 2,
+      ease: "power3.inOut",
+      stagger: {
+        amount: 0.1,
+      },
+    });
+  };
+
+  const staggerText = (node1, node2, node3) => {
+    gsap.from([node1, node2, node3], {
+      duration: 0.8,
+      y: 100,
+      delay: 0.1,
+      ease: "power3.inOut",
+      stagger: {
+        amount: 0.3,
+      },
+    });
+  };
+
+  return (
+    <div ref={(el) => (menu = el)} className="hamburger-menu">
+      <div
+        ref={(el) => (revealMenuBackground = el)}
+        className="menu-secondary-background-color"
+      ></div>
+      <div ref={(el) => (revealMenu = el)} className="menu-layer">
+        <div className="container">
+          <div className="wrapper">
+            <div className="menu-links">
+              <nav>
+                <ul>
+                  <li>
+                    <Link ref={(el) => (line1 = el)} to="/opportunities">
+                      Opportunities
+                    </Link>
+                  </li>
+                  <li>
+                    <Link ref={(el) => (line2 = el)} to="/solutions">
+                      Work
+                    </Link>
+                  </li>
+                  <li>
+                    <Link ref={(el) => (line3 = el)} to="/contact-us">
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hamburger;
---
+++ src/components/Header.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import { withRouter, Link } from "react-router-dom";
+import Hamburger from "./Hamburger";
+
+const Header = ({ history }) => {
+  //State for menu button
+  const [state, setState] = useState({
+    initial: false,
+    clicked: null,
+    menuName: "Menu",
+  });
+
+  // State for disabled button
+  const [disabled, setDisabled] = useState(false);
+
+  // Use effect for page changes
+  useEffect(() => {
+    //listen for page changes
+    history.listen(() => {
+      setState({ clicked: false, menuName: "Menu" });
+    });
+  });
+
+  const handleMenu = () => {
+    disableMenu();
+    if (state.initial === false) {
+      setState({
+        inital: null,
+        clicked: true,
+        menuName: "Close",
+      });
+    } else if (state.clicked === true) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Menu",
+      });
+    } else if (state.clicked === false) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Close",
+      });
+    }
+  };
+
+  // Determine if our menu button should be disabled
+  const disableMenu = () => {
+    setDisabled(!disabled);
+    setTimeout(() => {
+      setDisabled(false);
+    }, 1200);
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <div className="wrapper">
+          <div className="inner-header">
+            <div className="logo">
+              <Link to="/">Adarsh Goldar</Link>
+            </div>
+            <div className="menu">
+              <Link to="/" className="list">
+                Home
+              </Link>
+              <Link to="/" className="list">
+                About
+              </Link>
+              <button disabled={disabled} onClick={handleMenu}>
+                Menu
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Hamburger state={state} />
+    </header>
+  );
+};
+export default withRouter(Header);
